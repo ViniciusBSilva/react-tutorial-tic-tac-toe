2b19a9dc33d591f760cf3b16d075363cedc5b71e
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-02-14 18:48:26 -0300

    function calculateWinner

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ class Board extends React.Component {
 
     }
 
-    handleClick(i){
+    handleClick(i) {
         const squares = this.state.squares.slice();
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
@@ -105,3 +105,23 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let index = 0; index < array.length; index++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
